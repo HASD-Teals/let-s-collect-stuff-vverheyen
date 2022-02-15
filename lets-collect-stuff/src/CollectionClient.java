439664ecbe30cad413223e5076e06f41beba7ee3
vverheyen <90403002+vverheyen@users.noreply.github.com>
2022-02-15 10:01:43 -0600

    Update CollectionClient.java

--- lets-collect-stuff/src/CollectionClient.java
+++ lets-collect-stuff/src/CollectionClient.java
@@ -1,8 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CollectionClient {
     public static void main(String[] args) throws Exception {
-        // Add your collection ArrayList here..
-        // e.g. List<Collectable> collection = new ArrayList<Collectable>();
 
+        //ROCK COLLECTION
+        List<Collectable> collection = new ArrayList<Collectable>();
+
+        Collectable rock = new Collectable("rock",2,"scratched",1.00,"common");
+        collection.add(rock);
         // Add collection methods here to manage and manipulate the collection
     }
 }
