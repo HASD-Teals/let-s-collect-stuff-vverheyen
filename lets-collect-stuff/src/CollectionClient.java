aef5af49b06f02d0da582447dc030c20abd2dbc0
vverheyen <90403002+vverheyen@users.noreply.github.com>
2022-02-24 10:04:14 -0600

    Update CollectionClient.java

--- lets-collect-stuff/src/CollectionClient.java
+++ lets-collect-stuff/src/CollectionClient.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class CollectionClient {
     public static Scanner sc = new Scanner(System.in).useDelimiter(System.lineSeparator());
@@ -13,7 +11,7 @@ public class CollectionClient {
         Collectable rock = new Collectable("rock", "red brown", 5, "scratched", 1.0);
         Collectable calcite = new Collectable("calcite", "orange", 2, "shiny", 4.0);
         Collectable obsidian = new Collectable("obsidian", "black", 4, "glassy", 5.0);
-        Collectable emerald = new Collectable("emerald","green", 1, "brilliant", 200.0);
+        Collectable emerald = new Collectable("emerald", "green", 1, "brilliant", 200.0);
         Collectable carnelian = new Collectable("carnelian", "red", 3, "chunky", 4.0);
 
         collection.add(rock);
@@ -21,16 +19,18 @@ public class CollectionClient {
         collection.add(obsidian);
         collection.add(emerald);
         collection.add(carnelian);
-        
-        collectionSearch(collection);
+        add(collection, "lapiz lazuli", "blue", 3, "pristine", 7.00);
+        displayCollection(collection);
 
-        // Add collection methods here to manage and manipulate the collection
     }
-    //UNDER CONSTRUCTION
-    public static void add(List<Collectable> a, Collectable name, String color, int size, String condition, double cost) {
-        //name = new Collectable(name, color, size, condition, cost);
-        //a.add(Collectable);
+
+    public static void add(List<Collectable> a, String name, String color, int size, String condition,
+            double cost) {
+                Collectable bob = new Collectable(name, color, size, condition, cost);
+                a.add(bob);
+        
     }
+
     public static void sell(List<Collectable> a, int index) {
         a.remove(index);
     }
@@ -46,46 +46,52 @@ public class CollectionClient {
         }
         System.out.println("This collection's value is " + num);
     }
+
     public static void displayItem(List<Collectable> a, int index) {
         System.out.println(a.get(index));
         System.out.println();
     }
+
     public static void displayCollection(List<Collectable> a) {
-        for (int i=0; i<a.size(); i++) {
+        for (int i = 0; i < a.size(); i++) {
             System.out.println(a.get(i));
         }
         System.out.println();
     }
 
-    //UNDER CONSTRUCTION
     public static void collectionSearch(List<Collectable> a) {
-        System.out.println("Are you searching by int or String? ");
-        String b = sc.next();
-        if (b == "int") {
+        System.out.println("Are you searching by 1. int or 2. String? ");
+        int b = sc.nextInt();
+        if (b == 1) {
             System.out.println("Enter your search term: ");
             int c = sc.nextInt();
             for (int i = 0; i < a.size(); i++) {
                 int index = 0;
-                if (c == a.get(i).getSize()) { index = i; }
-                if (c == a.get(i).getCost()) { index = i; }
-                if (index > 0 ) {
+                if (c == a.get(i).getSize()) {
+                    index = i;
+                }
+                if (c == a.get(i).getCost()) {
+                    index = i;
+                }
+                if (index > 0) {
                     System.out.println(a.get(index));
                 }
             }
         }
 
-        if (b == "String") {
+        if (b == 2) {
             System.out.println("Enter your search term: ");
             String c = sc.next();
             for (int i = 0; i < a.size(); i++) {
                 int index = 0;
-                if (b == a.get(i).getName()) {
+
+                if (a.get(i).getName().equals(c)) {
                     index = i;
                 }
-                if (b == a.get(i).getCondition()) {
+                if (a.get(i).getCondition().equals(c)) {
                     index = i;
                 }
-                if (b == a.get(i).getColor()) {
+                if (a.get(i).getColor().equals(c)) {
                     index = i;
                 }
                 if (index > 0) {
@@ -94,28 +100,81 @@ public class CollectionClient {
 
             }
         }
-        sc.close();
     }
 
-    //ALSO UNDER CONSTRUCTION
-    public static void collectionSort(List<Collectable> a, String property) {
+    public static void collectionSort(List<Collectable> a) {
         System.out.println("What property would you like to sort by?");
         System.out.println("1. name, 2. color, 3. size, 4. condition, 5. cost");
         int num = sc.nextInt();
-            if (num == 1) {
-            
+        if (num == 1) {
+            for (int i = 0; i < a.size(); i++) {
+                for (int j = 0; j < a.size(); j++) {
+                    int b = a.get(i).getName().compareTo(a.get(j).getName());
+                    if (b < 0) {
+                        Collectable temp = a.get(i);
+                        a.set(i, a.get(j));
+                        a.set(j, temp);
+                    }
+                }
             }
-            if (num == 2) {
+            displayCollection(a);
+        }
 
+        if (num == 2) {
+            for (int i = 0; i < a.size(); i++) {
+                for (int j = 0; j < a.size(); j++) {
+                    int b = a.get(i).getColor().compareTo(a.get(j).getColor());
+                    if (b < 0) {
+                        Collectable temp = a.get(i);
+                        a.set(i, a.get(j));
+                        a.set(j, temp);
+                    }
+                }
             }
-            if (num == 3) {
+            displayCollection(a);
+        }
 
+        if (num == 3) {
+            for (int i = 0; i < a.size(); i++) {
+                for (int j = 0; j < a.size(); j++) {
+                    if (a.get(i).getSize() < a.get(j).getSize()) {
+                        Collectable temp = a.get(i);
+                        a.set(i, a.get(j));
+                        a.set(j, temp);
+                    }
+                }
             }
-            if (num == 4) {
+            displayCollection(a);
+        }
 
+        if (num == 4) {
+            for (int i = 0; i < a.size(); i++) {
+                for (int j = 0; j < a.size(); j++) {
+                    int b = a.get(i).getCondition().compareTo(a.get(j).getCondition());
+                    if (b < 0) {
+                        Collectable temp = a.get(i);
+                        a.set(i, a.get(j));
+                        a.set(j, temp);
+                    }
+                }
             }
-            if (num == 5) {
+            displayCollection(a);
+        }
 
+        if (num == 5) {
+            for (int i = 0; i < a.size(); i++) {
+                for (int j = 0; j < a.size(); j++) {
+                    if (a.get(i).getCost() < a.get(j).getCost()) {
+                        Collectable temp = a.get(i);
+                        a.set(i, a.get(j));
+                        a.set(j, temp);
+                    }
+                }
             }
+            displayCollection(a);
+        }
+        sc.close();
     }
+
 }
+
